7e4ca8dbe95b837f482226994caacb37e4d4a3b7
pjaulkar <padmaa.jaulkar@gmail.com>
2020-04-21 18:08:50 +0530

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -10,10 +10,10 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql, values=(), commit='FALSE', single='FALSE'):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
-    if commit == 'TRUE':
+    if commit == True:
         results = connection.commit()
     else:
         results  = cursor.fetchone() if single else cursor.fetchall()
